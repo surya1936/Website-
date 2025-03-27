f3a45edf0ecd060335f290191f85df160d7a3b3c
surya1936 <sd9136000@gmail.com>
2025-03-27 15:44:29 +0530

    js changes

--- html folder/Interaction.js
+++ html folder/Interaction.js
@@ -2,6 +2,26 @@ function toggleMenu() {
     document.querySelector(".menu-icon").classList.toggle("active");
     document.getElementById("sideMenu").classList.toggle("active");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    let lastScrollTop = 0;
+    const header = document.querySelector(".header");
+
+    window.addEventListener("scroll", function () {
+        let scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+        if (scrollTop > lastScrollTop) {
+            // Scrolling Down - Hide Header
+            header.classList.add("hidden");
+        } else {
+            // Scrolling Up - Show Header
+            header.classList.remove("hidden");
+        }
+
+        lastScrollTop = scrollTop;
+    });
+});
+
 const slider = document.querySelector('.product-slider');
 
 // Pause animation on hover (desktop)
